c4ae7ad7e3693a5d0a8dbe3c097e29589e0db4c8
vpm789 <vpm789@gmail.com>
2023-02-06 20:29:54 +0300

    task1-4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+//        task5();
     }
+
+    public static void task1() {
+        System.out.println("Задача №1");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task2() {
+        System.out.println("");
+        System.out.println("Задача №2");
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task3() {
+        System.out.println("");
+        System.out.println("Задача №3");
+        for (int i = 0; i <= 17; i = i + 2) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task4() {
+        System.out.println("");
+        System.out.println("Задача №4");
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+    }
+
+    /*public static void task5() {
+        System.out.println("");
+        System.out.println("Задача №5");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }*/
 }
