a35266f490fdc8415627de16cf8a8b6d1a5a3451
vpm789 <vpm789@gmail.com>
2023-02-06 20:37:13 +0300

    task5-7

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,9 @@ public class Main {
         task2();
         task3();
         task4();
-//        task5();
+        task5();
+        task6();
+        task7();
     }
 
     public static void task1() {
@@ -38,11 +40,27 @@ public class Main {
         }
     }
 
-    /*public static void task5() {
+    public static void task5() {
         System.out.println("");
         System.out.println("Задача №5");
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i + " год является високосным");
+        }
+    }
+
+    public static void task6() {
+        System.out.println("");
+        System.out.println("Задача №6");
+        for (int i = 7; i <= 98; i = i + 7) {
             System.out.println(i);
         }
-    }*/
+    }
+
+    public static void task7() {
+        System.out.println("");
+        System.out.println("Задача №7");
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.println(i);
+        }
+    }
 }
