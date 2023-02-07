267afc34eec49dec6c0397acfa9eccae0c75b0da
vpm789 <vpm789@gmail.com>
2023-02-07 15:59:54 +0300

    task5-7

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.IllegalFormatPrecisionException;
+
 public class Main {
     public static void main(String[] args) {
         task1();
@@ -7,6 +11,9 @@ public class Main {
         task5();
         task6();
         task7();
+        task8();
+        task9();
+        task10();
     }
 
     public static void task1() {
@@ -61,6 +68,40 @@ public class Main {
         System.out.println("Задача №7");
         for (int i = 1; i <= 512; i = i * 2) {
             System.out.println(i);
+        }
+    }
+
+    public static void task8() {
+        System.out.println("");
+        System.out.println("Задача №8");
+        int deposit = 29000;
+        int total = 0;
+        for (int i = 1; i <= 12; i++) {
+            total = total + deposit;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+        }
+    }
+
+    public static void task9() {
+        System.out.println("");
+        System.out.println("Задача №9");
+        int deposit = 29000;
+        double total = 0;
+        for (int i = 1; i <= 12; i++) {
+            total = total + (total * 0.12) / 12;
+            total = total + deposit;
+            BigDecimal bd = new BigDecimal(total);
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + bd.setScale(2, RoundingMode.HALF_UP) + " рублей");
+        }
+    }
+
+    public static void task10() {
+        System.out.println("");
+        System.out.println("Задача №10");
+        int total;
+        for (int i = 1; i <= 10; i++) {
+            total = 2 * i;
+            System.out.println("2*" + i + "=" + total);
         }
     }
 }
